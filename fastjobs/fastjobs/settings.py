3e7ef9e5a52bfb7eedc95dcb8c8e91363ce2a704
Josue Lopes <josue.lopes@iqnus.com.br>
2022-05-13 13:09:26 -0300

    Update settings.py

--- fastjobs/fastjobs/settings.py
+++ fastjobs/fastjobs/settings.py
@@ -119,10 +119,12 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+if not DEBUG: 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 # Default primary key field type
